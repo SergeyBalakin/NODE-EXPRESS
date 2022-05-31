4e49dd8ae5cca685300c5b63824aa498554b46fb
Sergey <36591136+SergeyBalakin@users.noreply.github.com>
2022-05-31 19:36:03 +0300

    Test project_2

---
+++ keys/keys.dev.js
@@ -0,0 +1,9 @@
+module.exports = {
+  MONGODB_URI:
+    'mongodb+srv://sergey:v6xH5QjEcixDV5C@cluster0.4swhn.mongodb.net/shop',
+  SESSION_SECRET: 'some secret value',
+  SENDGRID_API_KEYS:
+    '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',
+  EMAIL_FROM: 'balakinsergey83@gmail.com',
+  BASE_URL: 'http://localhost:3000',
+}
